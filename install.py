ebeb7b4c1e1443eeb0f46a48e3cee37cbfd58765
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-04-14 22:13:15 -0400

    add installation file

---
+++ install.py
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+"""
+Warning! This installation script is like how those popular opensource project
+automate the installation:
+
+- `Oh-my-zsh <https://github.com/ohmyzsh/ohmyzsh/blob/master/oh-my-zsh.sh>`_
+- `HomeBrew <https://github.com/Homebrew/install/blob/master/install.sh>`_
+- `pyenv installer <https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer>`_
+
+If this installation script is hacked, it may harm your laptop.
+
+Usage:
+
+.. code-block:: bash
+
+    python3 -c "$(curl -fsSL https://raw.githubusercontent.com/MacHu-GWU/alfred-cdk-ref/main/install.py)"
+"""
+
+import json
+import shutil
+from pathlib import Path
+from urllib.request import urlopen
+from zipfile import ZipFile
+
+# Define variables
+# don't use Path(__file__).absolute().parent, this script is executed by `python3 -c (curl ...)`
+dir_here = Path.cwd().absolute()
+dir_home = Path.home()
+dir_alfred_fts = Path(dir_home, ".alfred-afwf", "afwf_fts_anything")
+dir_alfred_fts.mkdir(parents=True, exist_ok=True)
+
+data_zip_file = "cdk-data.zip"
+path_data_zip = Path(dir_here, data_zip_file)
+
+# ------------------------------------------------------------------------------
+path_cdk_python_data = Path(dir_alfred_fts, "cdkpython-data.json")
+path_cdk_python_settings = Path(dir_alfred_fts, "cdkpython-setting.json")
+path_cdk_python_index = Path(dir_alfred_fts, "cdkpython-whoosh_index")
+# ------------------------------------------------------------------------------
+
+release_url = (
+    "https://api.github.com/repos/MacHu-GWU/alfred-cdk-ref/releases?per_page=1"
+)
+print("Find the latest release asset ...")
+res = urlopen(release_url)
+response_data = json.loads(res.read().decode("utf-8"))
+browser_download_url = response_data[0]["assets"][0]["browser_download_url"]
+file = "/".join(browser_download_url.split("/")[-2:])
+print(f"Found {file!r}!")
+
+print(f"download {browser_download_url} to {dir_here} ...")
+with Path(dir_here, data_zip_file).open("wb") as f:
+    res = urlopen(browser_download_url)
+    f.write(res.read())
+
+print(f"extract {data_zip_file} ...")
+with ZipFile(str(path_data_zip), "r") as z:
+    z.extractall(str(dir_here))
+
+print(f"Copy setting and data file to {dir_alfred_fts} ...")
+
+for (
+    path_data,
+    path_setting,
+    path_whoosh_index,
+) in [
+    (
+        path_cdk_python_data,
+        path_cdk_python_settings,
+        path_cdk_python_index,
+    )
+]:
+    path_data.write_text(Path(dir_here, path_data.name).read_text())
+    path_setting.write_text(Path(dir_here, path_setting.name).read_text())
+    shutil.rmtree(str(path_whoosh_index), ignore_errors=True)
